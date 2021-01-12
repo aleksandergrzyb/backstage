08dbafcca3df1916495e502f77a1d6f81731e9cb
Johan Bååth <info@johanbaath.com>
2021-01-12 15:11:08 +0100

    fix plugin import

--- plugins/apple-developer-portal/src/index.ts
+++ plugins/apple-developer-portal/src/index.ts
@@ -13,4 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export { default } from './components/MembershipPage';
+export { plugin } from './plugin';
